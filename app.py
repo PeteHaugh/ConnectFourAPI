49d68c2dca742d58936026bc4a5f917c96b0079b
Peter Haugh <peter.ahaugh@gmail.com>
2022-09-11 16:06:06 +0100

    Fixed CORS issues

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import flask
 from flask import request, jsonify
+from flask_cors import CORS
 import json
 from connect_four import evaluate_board, game_is_over, has_won, make_board, minimax, select_space
 
@@ -8,8 +9,7 @@ from connect_four import evaluate_board, game_is_over, has_won, make_board, mini
 my_board = make_board()
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = True
-
+CORS(app)
 @app.route('/')
 def home():
     return '<h1>Connect 4!</h1>'
@@ -18,17 +18,19 @@ def home():
 @app.route('/start', methods=['GET'])
 def api_start():
     my_board = make_board()
-    return f'{my_board}'
+    response = jsonify(message=f"Simple server is running, {my_board}")
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
     
 #route to move the piece for the player two to the desired column number,
 #if the column is invalid return invalid else the move is valid,
 #if there are four connecting pieces return Winner	
 
-@app.route('/move/<col>', methods=['GET', 'POST'])
+@app.route('/move/<col>', methods=['GET'])
 def make_move(col):
     # Check move is valid
 
-    move = int(col)
+    move = int(float(col))
 
     #The "O" player finds their best move
     valid = select_space(my_board, move, "O") #IF true it worked, if false it is invalid
@@ -39,18 +41,21 @@ def make_move(col):
      #The "X" player finds their best move.
         result = minimax(my_board, True, 4, -float("Inf"), float("Inf"), evaluate_board)
         select_space(my_board, result[1], "X")
-        return f'{my_board}'
+        status = f'{my_board}'
     elif has_won(my_board, "X"):
-        return "X won!"
+        status =  "X won!"
     elif has_won(my_board, "O"):
-        return "O won!"
+        status =  "O won!"
     else:
-        return "It's a tie!"
+        status =  "It's a tie!"
+
+    response = jsonify(message=f"{status}")
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
     
 def main():
-    
-    app.run()
+    app.run(host="0.0.0.0", port="5000", debug=True)
 
 
 if __name__ == "__main__":
