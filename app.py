b80f83517b4d9411808c1e52f4f5ce5fad9533b6
Peter Haugh <peter.ahaugh@gmail.com>
2022-10-18 15:36:17 +0100

    Results bug fixes

--- app.py
+++ app.py
@@ -11,39 +11,51 @@ board = make_board()
 app = flask.Flask(__name__)
 CORS(app)
 
-#route to reset the board and start a new game
+# route to reset the board and start a new game
 @app.route('/start', methods=['GET'])
 def api_start():
     reset_board(board)
     status = "Board reset"
     return jsonify(board, f"{status}")
-    
+
 
 @app.route('/move/<col>', methods=['GET'])
 def make_move(col):
 
     move = int(float(col))
 
-    #The "O" player finds their best move
-    valid = select_space(board, move, "O") #IF true it worked, if false it is invalid
-    
-    #Check if you've won
+    # The "O" player finds their best move
+    # IF true it worked, if false it is invalid
+    valid = select_space(board, move, "O")
+
+    # Check if you've won
 
     if not game_is_over(board):
-     #The "X" player finds their best move.
-        result = minimax(board, True, 4, -float("Inf"), float("Inf"), evaluate_board)
+     # The "X" player finds their best move.
+        result = minimax(board, True, 4, -float("Inf"),
+                         float("Inf"), evaluate_board)
+        print(board)
         select_space(board, result[1], "X")
-        status = "Playing..."
+
+        if has_won(board, "X"):
+            status = "X won!"
+        elif has_won(board, "O"):
+            status = "O won!"
+        else:
+            status = "Playing..."
+
+        return jsonify(board, f"{status}", result[1])
+
     elif has_won(board, "X"):
-        status =  "X won!"
+        status = "X won!"
     elif has_won(board, "O"):
-        status =  "O won!"
+        status = "O won!"
     else:
-        status =  "It's a tie!"
+        status = "It's a tie!"
 
     return jsonify(board, f"{status}")
 
-    
+
 def main():
     app.run(host="0.0.0.0", port="5000", debug=True)
 
